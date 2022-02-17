7089eb63ef860c10fbfad005a44759bf08b4d1cb
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-17 16:12:59 -0300

    ajustando as rotas

--- withReactNative/src/App.tsx
+++ withReactNative/src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {Home} from './feature';
-
-const Stack = createNativeStackNavigator();
+import {Routes} from './routes';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
+      <Routes />
     </NavigationContainer>
   );
 };
---
+++ withReactNative/src/routes/stack.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {DetailProduct, Home, Products} from '../feature';
+
+const Stack = createNativeStackNavigator();
+
+const routes = () => {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Products" component={Products} />
+      <Stack.Screen name="DetailProduct" component={DetailProduct} />
+    </Stack.Navigator>
+  );
+};
+
+export default routes;
