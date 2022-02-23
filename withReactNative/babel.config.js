862460361568143c8e2fe7cfbf4935c0456fe92b
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-23 11:30:27 -0300

    configurando babel p/ ler os modulos

--- withReactNative/babel.config.js
+++ withReactNative/babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@architecture': './src/architecture',
+          '@components': './src/architecture/components',
+        },
+      },
+    ],
+  ],
+  ignore: ['**/*.spec.ts'],
 };
